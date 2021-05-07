3815f7681cd9a8029d48717ffc15b78fd64895a7
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-05-07 22:24:24 +0200

    Bootstrap Added 💻

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://opentdb.com/api.php?amount=10'
 const questionContainer = document.querySelector('.question')
+const loadingSpinner = document.querySelector('#spinner')
 let randomIndex = Math.floor(Math.random() * 10) //Get a random question based on index
 let totalPoints = 0
 
@@ -11,17 +12,18 @@ function fetchQuestions() {
 const getQuestions = async () => {
     const q = await fetchQuestions()
     questionContainer.innerHTML += `
-    <p id='question-text'>${JSON.stringify(q.results[randomIndex].question)}</p>
-    <p id='question-category'>${JSON.stringify(q.results[randomIndex].category)}</p>
+    <i class="fs-1 fw-bold fas fa-question"></i>
+    <p id='question-text fw-bold'>${JSON.stringify(q.results[randomIndex].question)}</p>
+    <p id='question-category' class='mt-1 fs-6 text-decoration-underline'>${JSON.stringify(q.results[randomIndex].category)}</p>
     <p id='total-points'>You have ${totalPoints} points</p>
     <div class='options'>
-    <button>${q.results[randomIndex].correct_answer}</button>
+    <button class='shadow btn btn-dark btn-block mt-1 d-flex flex-column justify-content-center align-items-center vw-100'>${q.results[randomIndex].correct_answer}</button>
     </div>
     ` //Create correct answer button
     const optionsContainer = document.querySelector('.options')
     for (let i = 0; i < q.results[randomIndex].incorrect_answers.length; i++) {
         optionsContainer.innerHTML += `
-        <button>${q.results[randomIndex].incorrect_answers[i]}</button>
+        <button class='shadow-lg btn btn-dark btn-block mt-1 d-flex flex-column justify-content-center align-items-center vw-100'>${q.results[randomIndex].incorrect_answers[i]}</button>
         `
     } //Create incorrect answers buttons
     const optionsButtons = document.querySelectorAll('.options button')
@@ -46,7 +48,7 @@ const getQuestions = async () => {
         '#37123C', //Mitology
         '#FB5012', //Sports
         '#0A369D', //Geography
-        'E9DF00', //History
+        '#E9DF00', //History
         '#363537', //Politics
         '#730071', //Celebrities
         '#093824',//Animals
@@ -79,24 +81,35 @@ const getQuestions = async () => {
     } else if (q.results[randomIndex].category.includes(questionsCategories[8])) {
         questionContainer.style.backgroundColor = questionColors[8]
         questionContainer.style.color = '#fff'
+    } else if (q.results[randomIndex].category.includes(questionsCategories[9])) {
+        questionContainer.style.backgroundColor = questionColors[9]
+        questionContainer.style.color = '#fff'
+    } else if (q.results[randomIndex].category.includes(questionsCategories[10])) {
+        questionContainer.style.backgroundColor = questionColors[10]
+        questionContainer.style.color = '#fff'
     }
     //Verify the answers
     optionsButtons.forEach(button => button.addEventListener('click', () => {
         if (button.textContent === q.results[randomIndex].correct_answer) {
-            button.style.backgroundColor = 'green'
+            button.classList.remove('btn-dark')
+            button.classList.add('btn-success')
             button.style.pointerEvents = 'none'
             totalPoints += 1
+            loadingSpinner.classList.remove('visually-hidden')
             setTimeout(() => {
                 questionContainer.innerHTML = ''
+                loadingSpinner.classList.add('visually-hidden')
                 getQuestions()
             }, 1000)
-            console.log(totalPoints)
         } else {
-            button.style.backgroundColor = 'red'
+            button.classList.remove('btn-dark')
+            button.classList.add('btn-danger')
             button.color = '#fff'
             totalPoints = 0
+            loadingSpinner.classList.remove('visually-hidden')
             setTimeout(() => {
                 questionContainer.innerHTML = ''
+                loadingSpinner.classList.add('visually-hidden')
                 getQuestions()
             }, 1000)
         }
